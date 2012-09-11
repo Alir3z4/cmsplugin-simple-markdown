3050def98f42ee7273c135f4ab47e51b9bc92f6f
Alireza Savand <alireza.savand@gmail.com>
2012-09-11 04:38:38 +0400

    Added SimpleMarkdownPlugin

--- cmsplugin_simple_markdown/models.py
+++ cmsplugin_simple_markdown/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from cms.models import CMSPlugin
 
-# Create your models here.
+class SimpleMarkdownPlugin(CMSPlugin):
+    mardown_text = models.TextField(verbose_name=_('markdown body'))
+
+    def __unicode__(self):
+        return self.mardown_text
