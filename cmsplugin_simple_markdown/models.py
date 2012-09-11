68674727334caa0fac7a921f62a2bd61f342878d
Alireza Savand <alireza.savand@gmail.com>
2012-09-11 05:06:23 +0400

    better stuff on here!

--- cmsplugin_simple_markdown/models.py
+++ cmsplugin_simple_markdown/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
-from cms.models import CMSPlugin
+from cms.models.pluginmodel import CMSPlugin
 
 class SimpleMarkdownPlugin(CMSPlugin):
     markdown_text = models.TextField(verbose_name=_('markdown body'))
