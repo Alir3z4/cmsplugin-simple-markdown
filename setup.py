2527d65b08f46e44eda707b50aa3a7b58d9a952a
Alireza Savand <alireza.savand@gmail.com>
2012-09-12 16:06:57 +0400

    fix the license stuff ;)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version=".".join(map(str, __import__('cmsplugin_simple_markdown').__version__)),
     packages=find_packages(exclude=['django_cmsplugin_simple_markdown']),
     url='https://www.github.com/Alir3z4/cmsplugin-simple-markdown',
-    license=open('LICENSE').read(),
+    license='LICENSE',
     author='Alireza Savand',
     author_email='alireza.savand@gmail.com',
     description='A plugin for django-cms that provides just a markdown plugin and nothing more.',
