2996b944a8e2b531e0dbbb3d1d34dd957d828d9b
Alireza Savand <alireza.savand@gmail.com>
2012-09-12 15:09:45 +0400

    updated setup.py with more info

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='cmsplugin_simple_markdown',
+    name='cmsplugin-simple-markdown',
     version=".".join(map(str, __import__('cmsplugin_simple_markdown').__version__)),
-    packages=[find_packages(exclude='django_cmsplugin_simple_markdown')],
+    packages=find_packages(exclude=['django_cmsplugin_simple_markdown']),
     url='https://www.github.com/Alir3z4/cmsplugin-simple-markdown',
-    license='BSD',
+    license=open('LICENSE').read(),
     author='Alireza Savand',
     author_email='alireza.savand@gmail.com',
     description='A plugin for django-cms that provides just a markdown plugin and nothing more.',
+    long_description=open('README').read(),
     install_requires=['django', 'django-cms', 'markdown'],
     keywords=[
         'django',
-        'request',
-        'web'
+        'django-cms',
+        'web',
+        'cms',
+        'cmsplugin',
+        'plugin',
     ],
     platforms='OS Independent',
     provides=['cmsplugin_simple_markdown',],
