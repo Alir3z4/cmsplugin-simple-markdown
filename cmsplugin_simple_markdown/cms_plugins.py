8b594ba0d682a3ef252a6c70445ed590bcb40734
Philipp Bosch <hello@pb.io>
2013-02-07 18:15:01 +0100

    Textarea styling. Remove admin preview.

--- cmsplugin_simple_markdown/cms_plugins.py
+++ cmsplugin_simple_markdown/cms_plugins.py
@@ -1,14 +1,26 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
+from django import forms
 from django.utils.translation import ugettext_lazy as _
 from cmsplugin_simple_markdown.models import SimpleMarkdownPlugin
 from cms.utils.page_resolver import get_page_from_path
 import re
 
+
+class SimpleMarkdownCMSPluginForm(forms.ModelForm):
+    class Meta:
+        model = SimpleMarkdownPlugin
+        widgets = {
+            'markdown_text': forms.Textarea(attrs={'cols': 100, 'rows': 20, 'style': 'font-family: Monaco, monospace;'})
+        }
+
+
 class SimpleMarkdownCMSPlugin(CMSPluginBase):
     model = SimpleMarkdownPlugin
-    name = _('Simple Markdown')
+    name = _('Text (Markdown)')
     render_template = 'cmsplugin_simple_markdown/simple_markdown.html'
+    admin_preview = False
+    form = SimpleMarkdownCMSPluginForm
 
     def replace_links(self, markdown_text):
         def link_repl(match):
--- cmsplugin_simple_markdown/models.py
+++ cmsplugin_simple_markdown/models.py
@@ -3,7 +3,7 @@ from django.utils.translation import ugettext_lazy as _
 from cms.models.pluginmodel import CMSPlugin
 
 class SimpleMarkdownPlugin(CMSPlugin):
-    markdown_text = models.TextField(verbose_name=_('markdown body'))
+    markdown_text = models.TextField(verbose_name=_('text'))
 
     def __unicode__(self):
         return self.markdown_text
