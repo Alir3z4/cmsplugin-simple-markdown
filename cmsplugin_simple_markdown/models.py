f3ab7723be85653b39be24c3819cd5b59c119061
Alireza Savand <alireza.savand@gmail.com>
2013-05-16 23:59:28 +0400

    little bit more pep8 around!

--- cmsplugin_simple_markdown/models.py
+++ cmsplugin_simple_markdown/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from cms.models.pluginmodel import CMSPlugin
 
+
 class SimpleMarkdownPlugin(CMSPlugin):
     markdown_text = models.TextField(verbose_name=_('text'))
 
